5fc5a7e89a933b5ed92008ecb8b418cfe10582e2
PERSONIV\rico.bugtong <rico.gomez.bugtong@gmail.com>
2024-03-08 21:14:33 +0800

    update master-without-nav

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -15,7 +15,16 @@
 
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
-        @include('layouts.head-css')
+        @yield('css')
+
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+        <!-- Custom Css-->
+        <link href="{{ asset('assets/css/custom.css') }}" id="app-style" rel="stylesheet" type="text/css" />
     </head>
 
     @yield('body')
