5827541cbf97f97a330bb7937039037e8fa5f2e6
PERSONIV\rico.bugtong <rico.gomez.bugtong@gmail.com>
2025-05-15 20:48:39 +0800

    set dashboard default date filter based on user shift date

--- resources/views/pages/dashboard/filters.blade.php
+++ resources/views/pages/dashboard/filters.blade.php
@@ -1,24 +1,4 @@
 
-    {{-- <div class="col-lg-4">
-        <div class="card">
-            <div class="card-body">
-                <div class="input-group">
-                    <select class="form-control" name="slct_filter" id="slct_filter">
-                        <option value="all">All</option>
-                        <option value="daily">Daily</option>
-                        <option value="weekly">Weekly</option>
-                        <option value="monthly">Monthly</option>
-                        <option value="yearly">Yearly</option>
-                    </select>
-                    <div id="div_filter"></div>
-                    <span class="input-group-btn">
-                        <button type="button" class="btn btn-primary" id="btn_filter" data-toggle="tooltip" title="Filter Data" tabindex="-1"><span class="fa fa-filter"></span> Filter</button>
-                    </span>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
     <div class="col-lg-4 d-flex align-items-stretch">
         <div class="card w-100">
             <div class="card-body">
@@ -32,7 +12,7 @@
                         {{-- <option value="quarterly">Quarterly</option> --}}
                     </select>
                     <div id="div_filter">
-                        <input type="date" class="form-control mt-2" id="filter_option" value="{{ date('Y-m-d') }}" required />
+                        <input type="date" class="form-control mt-2" id="filter_option" value="{{ auth()->user()->shift_date ? date('Y-m-d', strtotime(auth()->user()->shift_date)) : date('Y-m-d') }}" required />
                     </div>
                     <span class="input-group-btn mt-2 ms-2">
                         <button type="button" class="btn btn-primary" id="btn_filter" data-toggle="tooltip" title="Filter Data" tabindex="-1">
