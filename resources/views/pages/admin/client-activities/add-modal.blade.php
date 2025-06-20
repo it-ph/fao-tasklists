2cf4b60f5d68a607a507c49b7b515a643bd80a17
PERSONIV\rico.bugtong <rico.gomez.bugtong@gmail.com>
2025-06-20 21:11:26 +0800

    apply fixed lists of function

--- resources/views/pages/admin/client-activities/add-modal.blade.php
+++ resources/views/pages/admin/client-activities/add-modal.blade.php
@@ -25,21 +25,21 @@ use App\Models\TaskPause;
                         <input type="text" class="form-control" name="schedule" required>
 
                         <label for="function" class="col-form-label custom-label"><strong>FUNCTION:<span class="important">*</span></strong></label>
-                        {{-- <select name="function" class="form-control">
+                        <select name="function" class="form-control">
                             <option value="">-- Select Function --</option>
                             <option value="Procure to Pay (P2P)">Procure to Pay (P2P)</option>
                             <option value="Order to Cash (O2C)">Order to Cash (O2C)</option>
                             <option value="Record to Report (R2R)">Record to Report (R2R)</option>
                             <option value="Client Admin">Client Admin</option>
                             <option value="Personiv Admin">Personiv Admin</option>
-                        </select> --}}
+                        </select>
 
-                        <input list="functions" id="comboBox" name="function" class="form-control" placeholder="Type or select a function">
+                        {{-- <input list="functions" id="comboBox" name="function" class="form-control" placeholder="Type or select a function">
                         <datalist id="functions">
                             @foreach ($functions as $function)
                                 <option value="{{ $function->function }}">
                             @endforeach
-                        </datalist>
+                        </datalist> --}}
 
                         <input type="hidden" class="form-control" name="agent_id" value="{{ \Request::get('user_id') }}">
                     </div>
